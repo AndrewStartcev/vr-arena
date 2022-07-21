b2bcb41e7ba6e4bf59b3c5322be454d89c906093
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2022-07-21 20:11:24 +0800

    add block game-price

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -33,28 +33,32 @@ $(document).ready(() => {
   };
 
   function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];
-    xDown = firstTouch.clientX;
-    yDown = firstTouch.clientY;
+    if (!$('body').hasClass('no-swipe')) {
+      const firstTouch = getTouches(evt)[0];
+      xDown = firstTouch.clientX;
+      yDown = firstTouch.clientY;
+    }
   };
 
   function handleTouchMove(evt) {
-    if (!xDown || !yDown) {
-      return;
-    }
+    if (!$('body').hasClass('no-swipe')) {
+      if (!xDown || !yDown) {
+        return;
+      }
 
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
+      var xUp = evt.touches[0].clientX;
+      var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
 
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-      if (xDiff > 0) {
-        menu.addClass(active);
-      } else {
-        menu.removeClass(active);
-      };
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+          menu.addClass(active);
+        } else {
+          menu.removeClass(active);
+        };
+      }
     }
   }
 
@@ -73,5 +77,26 @@ $(document).ready(() => {
       $('header').removeClass('header-bg');
     }
   }
+
+  function elementInViewport(el) {
+    var bounds = el.getBoundingClientRect();
+    return (
+      (bounds.top + bounds.height > 0) && // Елемент ниже верхней границы
+      (window.innerHeight - bounds.top > 0) && // Выше нижней
+      (bounds.left + bounds.width > 0) && // Правее левой
+      (window.innerWidth - bounds.left > 0)// Левее правой
+    );
+  }
+
+
+  document.addEventListener("scroll", (e) => {
+    var el = document.querySelector(".price-game");
+    var inViewport = elementInViewport(el);
+    if (inViewport) {
+      $('body').addClass('no-swipe')
+    } else {
+      $('body').removeClass('no-swipe')
+    }
+  })
 
 })
